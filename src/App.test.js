c39b53e0231b21dae27f458260e902b4504ed062
M N Ikhsan Mubaarak <126537753+baarak08@users.noreply.github.com>
2024-11-17 01:32:03 +0700

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Bender Force Saga title', () => {
-  render(<App />);
-  const titleElement = screen.getByRole('heading', { level: 1, name: /Bender Force Saga/i });
-  expect(titleElement).toBeInTheDocument();
-});
