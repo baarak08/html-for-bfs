a12eaf704b25bd753d355a1e51fd4f7c2a77003a
M N Ikhsan Mubaarak <126537753+baarak08@users.noreply.github.com>
2024-11-17 00:58:58 +0700

    "Add GitHub Pages deployment"

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Bender Force Saga title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByRole('heading', { level: 1, name: /Bender Force Saga/i });
+  expect(titleElement).toBeInTheDocument();
 });
